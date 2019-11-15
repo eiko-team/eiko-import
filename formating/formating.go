24dda74469725bda9c1abbf62b65b6013a5d6946
Tom Moulard <tom.moulard@epita.fr>
2019-11-15 12:41:04 +0100

    format: adding nested and arrays

--- formating/formating.go
+++ formating/formating.go
@@ -2,20 +2,75 @@ package formating
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"strconv"
 
 	"github.com/eiko-team/eiko/misc/structures"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func getFirstValue(data bson.M, keys []string) string {
 	for _, str := range keys {
+		if str == "" {
+			continue
+		}
 		if data[str] != nil {
 			return data[str].(string)
 		}
 	}
 	return ""
+}
+
+func getAllValues(data bson.M, keys []string) []string {
+	var res []string
+	for _, str := range keys {
+		if str == "" {
+			continue
+		}
+		if data[str] != nil {
+			if reflect.ValueOf(data[str]).Kind() == reflect.Slice {
+				for _, d := range data[str].(primitive.A) {
+					res = append(res, string(d.(string)))
+				}
+			}
+		}
+		if len(res) > 0 {
+			// Got a match
+			return res
+		}
+	}
+	return res
+}
+
+func getNestedValues(data bson.M, key string, keys []string,
+	selector string) string {
+	var res string
+	if key == "" || data[key] == nil {
+		return ""
+	}
+
+	d := data[key].(bson.M)
+	for _, str := range keys {
+		if str == "" {
+			continue
+		}
+		if d[str] != nil {
+			for k, val := range d[str].(bson.M) {
+				fmt.Println(k, val)
+			}
+			fmt.Println("\n", d[str].(bson.M),
+				"\n",
+				d[str].(bson.M)["sizes"],
+				d[str].(bson.M)[selector],
+				selector, "\n\n\n\n\n\n\n")
+			panic(d[str].(bson.M)[selector].(string))
+			return string(d[str].(bson.M)[selector].(string))
+		}
+	}
+	return res
 }
 
 func getName(data bson.M) string {
@@ -27,11 +82,11 @@ func getCompagnyName(data bson.M) string {
 }
 
 func getAdditive(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{"additives_n", "additives_tags", "additives_old_n", "additives_old_tags", "additives_original_tags", "additives_prev_original_tags", "additives_debug_tags"})
 }
 
 func getAllergen(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{"allergens", "allergens_from_ingredients", "allergens_from_user", "allergens_tags", "allergens_hierarchy"})
 }
 
 func getEnergie(data bson.M) float64 {
@@ -101,43 +156,52 @@ func getAlcool(data bson.M) float64 {
 }
 
 func getBack(data bson.M) string {
-	return ""
+	// return getFirstValue(data, []string{"images.ingredients_fr"})
+	// str := getNestedValues(data, "images", []string{"ingredients_fr"}, "full")
+	// if str != "" {
+	// 	panic(str)
+	// }
+	str := getFirstValue(data, []string{"image_url", "image_small_url"})
+	if str != "" {
+		panic(str)
+	}
+	return str
 }
 
 func getComposition(data bson.M) string {
-	return ""
+	return getFirstValue(data, []string{""})
 }
 
 func getFront(data bson.M) string {
-	return ""
+	return getFirstValue(data, []string{""})
 }
 
 func getManufacturing(data bson.M) string {
-	return ""
+	return getFirstValue(data, []string{""})
 }
 
 func getCode(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func getCategories(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func getTags(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func getPackaging(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func getIngredient(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func getVitamins(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func getNutriScore(data bson.M) string {
@@ -157,7 +221,7 @@ func getMLitre(data bson.M) int {
 }
 
 func getLabel(data bson.M) []string {
-	return []string{""}
+	return getAllValues(data, []string{""})
 }
 
 func BsonToString(data bson.M) (string, error) {
